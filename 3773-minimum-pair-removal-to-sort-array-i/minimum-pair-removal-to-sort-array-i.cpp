4d061ec5414664924b6c52075d83e4d4b14a2e98
Devanshyadv <147366834+Devanshyadv@users.noreply.github.com>
2026-01-22 18:03:51 +0530

    Time: 3 ms (40.33%) | Memory: 29.3 MB (19.02%) - LeetSync

--- 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool check(vector<int>arr){
+    bool check(vector<int>&arr){
         int n=arr.size();
         for(int i=1;i<n;i++){
             if(arr[i]<arr[i-1]){
