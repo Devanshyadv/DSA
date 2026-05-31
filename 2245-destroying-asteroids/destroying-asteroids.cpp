6db45a47360c24ef7f99360b58fef33af67750d2
Devanshyadv <147366834+Devanshyadv@users.noreply.github.com>
2026-05-31 23:22:07 +0530

    Time: 31 ms (66.42%) | Memory: 106.6 MB (81.61%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        int n=asteroids.size();
+        sort(asteroids.begin(),asteroids.end());
+        long long tmass=mass;
+        for(int i=0;i<n;i++){
+            if(asteroids[i]<=tmass){
+                tmass+=asteroids[i];
+            }
+            if(asteroids[i]>tmass){
+                return false;
+            }
+        }
+        return true;
+    }
+};
